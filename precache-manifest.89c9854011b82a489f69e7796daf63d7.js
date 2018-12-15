81d8daeb65f5283f6f48f3776933812629531fc0
plugshubham <shbhkumar6@gmail.com>
2018-12-15 13:54:10 +0530

    Updates

---
+++ precache-manifest.89c9854011b82a489f69e7796daf63d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0914b53e62f7f4a97c4d",
+    "url": "/react-news-app/static/js/runtime~main.0914b53e.js"
+  },
+  {
+    "revision": "6a3aba30acf38188c455",
+    "url": "/react-news-app/static/js/main.6a3aba30.chunk.js"
+  },
+  {
+    "revision": "ce48fc6997b138e6dc59",
+    "url": "/react-news-app/static/js/1.ce48fc69.chunk.js"
+  },
+  {
+    "revision": "6a3aba30acf38188c455",
+    "url": "/react-news-app/static/css/main.5d7a81d4.chunk.css"
+  },
+  {
+    "revision": "ce48fc6997b138e6dc59",
+    "url": "/react-news-app/static/css/1.00a00657.chunk.css"
+  },
+  {
+    "revision": "178b565c0a7a0006475d506df8332a0c",
+    "url": "/react-news-app/index.html"
+  }
+];
